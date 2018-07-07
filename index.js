d69119e7823ccca8fac90736df88da40bfacb4da
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-07 11:15:49 +0530

    yargs for command arguments

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ console.log("starting app");
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 // fs.appendFile('greetings.txt', `hello ${user.username} ! you are ${notes.age} !`, (error) => {});
 // console.log(process.argv);
+const argv = yargs.argv;
 var command = process.argv[2];
-console.log(command);
+console.log("process : ", process.argv);
+console.log("yargs : ", argv);
