aeecdfd7d8330a9b020e95699e8c096c6cf14f55
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-07 12:26:28 +0530

    fetch and save

--- notes.js
+++ notes.js
@@ -2,23 +2,30 @@ console.log('starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+    try {
+        var noteString = fs.readFileSync('./notes-data.json');
+        return JSON.parse(noteString);
+    } catch (e) {
+        console.log("creating new file for adding notes");
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('./notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
     var notes = [];
     var note = {
         title,
         body
     };
-    try {
-        var noteString = fs.readFileSync('./notes-data.json');
-        notes = JSON.parse(noteString);
-    } catch (e) {
-        console.log("creating new file for adding notes");
-    }
-
     var duplicateNotes = notes.filter((note) => note.title === title);
     if (duplicateNotes.length == 0) {
         notes.push(note);
-        fs.writeFileSync('./notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
     } else {
         console.log('duplicate note already exists');
     }
